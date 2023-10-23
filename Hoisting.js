d28fbf44ae82da0a988008eec5bf644312b70471
AlvaroVilladangos <alvaro.villadangos@iesdonana.org>
2023-10-23 12:17:10 +0200

    ejercicio udemy

---
+++ Hoisting.js
@@ -0,0 +1,11 @@
+//hoisting
+//(podemos usar una variable, y despues declararla)
+
+
+//var x; //1. declarar la variables
+x =10; //2 Inicializamos la variable
+//y =5 //al usar let no aplica el concepto de hoisting
+console.log(x);
+
+var x;
+let y;
